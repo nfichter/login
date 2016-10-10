983768a34ea7a9c542045eff816584a2b6103306
nfichter <nfichter@stuy.edu>
2016-10-10 14:44:16 -0400

    commented register.py

--- utils/register.py
+++ utils/register.py
@@ -1,14 +1,17 @@
 import hashlib
 
-def register(usernameGiven,passwordGiven):
+def register(usernameGiven,passwordGiven): #returns True if successfully registered, False if the username exists already
+    #imports file
     csv = open("data/users.csv","r+")
     users = csv.read().split("\n")
 
+    #checks if any of the users are the same as the username given
     for combo in users:
         username = combo.split(",")[0]
         if usernameGiven == username:
             return False
-        
+
+    #otherwise, hashes the password and puts it into the file
     hashObj = hashlib.sha1()
     hashObj.update(passwordGiven)
         
