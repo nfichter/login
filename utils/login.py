bfa3148c2faf9675df25c29a343f3fbdd319d5de
nfichter <nfichter@stuy.edu>
2016-10-10 14:43:15 -0400

    commented login.py

--- utils/login.py
+++ utils/login.py
@@ -1,13 +1,15 @@
 import hashlib
 
 def login(usernameGiven,passwordGiven): #returns 0 if user does not exist, 1 if wrong password, 2 if correct password
+    #open and delete last line
     csv = open("data/users.csv","r+")
     users = csv.read().split("\n")
     del users[len(users)-1]
     usersDict = {}
 
     userExists = False
-    
+
+    #check if the username exists while going through and turning the list of user,pw into a dictionary
     for combo in users:
         username = combo.split(",")[0]
         password = combo.split(",")[1]
@@ -17,12 +19,12 @@ def login(usernameGiven,passwordGiven): #returns 0 if user does not exist, 1 if 
             userExists = True
 
     if not userExists:
-        return 0
+        return 0 #user does not exist
             
     hashObj = hashlib.sha1()
     hashObj.update(passwordGiven)
 
     if hashObj.hexdigest() == usersDict[usernameGiven]:
-        return 2
+        return 2 #password correct
     else:
-        return 1
+        return 1 #password incorrect
